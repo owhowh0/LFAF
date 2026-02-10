623a58e0fad7e04894a86c7d5de2b18d42d0cb1c
Ilie Covali <owhowh_0@proton.me>
2026-02-10 12:19:25 +0200

    add grammar to initializator

--- Lab1/grammar.py
+++ Lab1/grammar.py
@@ -1,5 +1,19 @@
 class Grammar:
 
+    def __init__(self):
+        self.VN = {"S", "B", "D", "Q"}
+
+        self.VT = {"a", "b", "c", "d"}
+
+        self.S = "S"
+
+        self.P = {
+            "S": ["aB", "bB"],
+            "B": ["cD"],
+            "D": ["dQ", "a"],
+            "Q": ["bB", "dQ"]
+        }
+
     def generate_string():
         pass
 
